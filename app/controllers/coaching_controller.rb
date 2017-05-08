71101d590463a7a7f074671f6327aaabf15e2d7f
Adalberto GONZALEZ <adalg08@gmail.com>
2017-05-08 12:39:25 +0200

    1st rails project finished

---
+++ app/controllers/coaching_controller.rb
@@ -0,0 +1,16 @@
+class CoachingController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @question = params["query"].capitalize
+    if @question.end_with?("?")
+      @answer = "Silly question, get dressed and go to work!"
+    elsif @question == "I am going to work right now!"
+      @answer = ""
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+
+  end
+end
